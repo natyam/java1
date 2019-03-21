2a5870d8ca20f550aeeafb9f37b15c22f7f3349d
natyam <48775454+natyam@users.noreply.github.com>
2019-03-20 22:10:05 -0300

    Add files via upload

---
+++ IF.java
@@ -0,0 +1,16 @@
+package clase2if;
+
+public class IF {
+	public static void main(String[] args){
+		int valor =1;
+		if (valor<0){
+			//bloque de codigo si se cumple la condicion
+			System.out.println("Valor es negativo");
+		}
+		else
+			{
+			System.out.println("Valor es positivo o cero ");
+			}
+	}
+
+}
